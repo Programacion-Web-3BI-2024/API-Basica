0a77ebae1dd4200b08d8f9a03cb41b76bf7808ee
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2024-06-11 18:23:37 -0300

    Agregamos modificar una pizza. Y unos cuantos comentarios

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -28,4 +28,19 @@ class PizzaController extends Controller
         $pizza -> delete();
         return [ 'mensaje' => 'Pizza eliminada' ];
     }
+
+    public function Modificar(Request $request, $id){
+        $pizza = Pizza::findOrFail($id);
+
+        // Por mas que el metodo sea PUT, 
+        // Laravel no diferencia entre PUT y POST 
+        // para acceder a los parametros recibidos,
+        // siempre se usa $request -> post("nombre_del_parametro"). 
+
+        $pizza -> nombre = $request -> post("nombre"); 
+        $pizza -> precio = $request -> post("precio");
+        $pizza -> save();
+
+        return $pizza; // Por estandar REST, retornamos el modelo modificado
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,6 @@ Route::post('/pizza', [PizzaController::class, 'Crear']);
 Route::get('/pizza', [PizzaController::class, 'ListarTodas']);
 Route::get('/pizza/{d}', [PizzaController::class, 'ListarUna']);
 Route::delete('/pizza/{d}', [PizzaController::class, 'Eliminar']);
+Route::put('/pizza/{d}', [PizzaController::class, 'Modificar']);
 
 
